e5360f80f7d0107c526cf3690842cd68ece97700
Alberto C. Ríos <albertoimpl@gmail.com>
2022-09-06 17:20:10 +0200

    Initial commit

---
+++ native-trivy-demo/src/main/java/com/example/nativetrivydemo/NativeTrivyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.nativetrivydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class NativeTrivyDemoApplication {
+
+		public static void main(String[] args) {
+				SpringApplication.run(NativeTrivyDemoApplication.class, args);
+		}
+
+}
---
+++ native-trivy-demo/src/test/java/com/example/nativetrivydemo/NativeTrivyDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.nativetrivydemo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class NativeTrivyDemoApplicationTests {
+
+		@Test void contextLoads() {
+		}
+
+}
